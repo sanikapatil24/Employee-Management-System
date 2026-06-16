95327cd7e0fa15f3aac274a102176f891c7d1b32
sanikapatil24 <patilsanika0103@gmail.com>
2026-06-16 23:40:02 +0530

    Initial Commit

---
+++ EmployeeMgtBackend/src/main/java/com/sit/EmployeeMgtBackendApplication.java
@@ -0,0 +1,12 @@
+package com.sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgtBackendApplication.class, args);
+	}
+}
---
+++ EmployeeMgtBackend/src/test/java/com/sit/EmployeeMgtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
